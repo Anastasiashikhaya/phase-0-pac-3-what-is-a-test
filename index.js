1142dd82d931d73b91b5167ec7083640c8afe83f
Anastasia Wakoli <anastasiashikhaya@gmail.com>
2025-03-10 17:36:55 +0300

    phase o pac 3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
-const message = `${name} is ${height} inches tall`;
+const name = "Susan"; // Change "Joe" to "Susan"
+const height = 35; // Set a value less than 40
+const message = `${name} is ${height} inches tall`; // Ensure message reflects changes
 
-module.exports = {name, height, message};
+module.exports = { name, height, message };
